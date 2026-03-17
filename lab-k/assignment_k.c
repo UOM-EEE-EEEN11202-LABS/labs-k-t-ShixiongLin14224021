48fe61873b2478449b96024c75acd3cca23a2909
ShixiongLin14224021 <shixiong.lin@student.manchester.ac.uk>
2026-03-17 23:21:52 +0000

    lab_k

--- lab-k/assignment_k.c
+++ lab-k/assignment_k.c
@@ -6,6 +6,22 @@ int main(void) {
   scanf("%lf", &height);
 
   // Your code in this section
+  // store my email address
+  char email[] = "shixiong.lin@student.manchester.ac.uk";
+
+  // values for cylinder volume
+  double radius = 2.2;
+  double pi = 3.14159;
+  double volume = 0;
+
+  // check for invalid height
+  if (height < 0) {
+    printf("Error");
+    return 1;
+  }
+
+  // calculate the volume
+  volume = pi * radius * radius * height;
 
   // Do not edit below here
   printf("%s\n", email);
