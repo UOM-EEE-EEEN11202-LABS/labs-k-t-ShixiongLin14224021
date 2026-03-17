4391113a805cac9ec43493fc11b4d2d0529da644
ShixiongLin14224021 <shixiong.lin@student.manchester.ac.uk>
2026-03-17 00:12:36 +0000

    lab_k

---
+++ lab-k/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  double distance = 0;
+  double time = 0;
+  int speed = 0;
+
+  distance = 100.0;
+  time = 9.58;
+  speed = distance / time;
+  return 0;
+}
--- lab-k/smallest_amount.c
+++ lab-k/smallest_amount.c
@@ -12,6 +12,26 @@ int main(void) {
   int num_fifties, num_twenties, num_tens, num_fives, num_ones;
 
   // Your code below here
+  // Solution. (Click the eye icon to show)
+  // Name goes here
+  //
+  // Aim
+  // Calculate the smallest number of pound notes / coins needed
+  // ----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+  // Your code below here
+  int amount_reduced_by_fifties, amount_reduced_by_twenties;
+  int amount_reduced_by_tens;
+  num_fifties = starting_amount / 50;
+  amount_reduced_by_fifties = starting_amount - (50 * num_fifties);
+  num_twenties = amount_reduced_by_fifties / 20;
+  amount_reduced_by_twenties = amount_reduced_by_fifties - (20 * num_twenties);
+  num_tens = amount_reduced_by_twenties / 10;
+  amount_reduced_by_tens = amount_reduced_by_twenties - (10 * num_tens);
+  num_fives = amount_reduced_by_tens / 5;
+  num_ones = amount_reduced_by_tens - (5 * num_fives);
 
   // Display results
   printf("Starting amount in pounds: %d\n", starting_amount);
